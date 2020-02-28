7032ef062987abe3cb2276b084e6e420c2964b5b
joshmaglione <joshmaglione@gmail.com>
2020-02-28 15:01:05 +0100

    Cone conditions and bug fixes
    Added cone conditions for commutative rings given by tensors. Also fixed
    some bugs with the GenFunc class.

--- src/ConeConditions.py
+++ src/ConeConditions.py
@@ -29,18 +29,18 @@ def _dot_product(u, v):
 # matrix M (to obtain the kth coefficient). 
 def _multiply_rows(P, i, j, M, UT=True):
     n = M.ncols()
-    varbs_i = _row_vector_vars(P, n, i)
-    varbs_j = _row_vector_vars(P, n, j)
+    varbs_i = _row_vector_vars(P, n, i, UT=UT)
+    varbs_j = _row_vector_vars(P, n, j, UT=UT)
     if UT:
         X = M[i:]
-        X_cols = [X[:,i] for i in range(n)]
+        X_cols = [X[:,i] for i in range(j, n)]
         u = map(lambda x: _dot_product(varbs_i, x), X_cols)
-        return _dot_product(_vec(u[j:]), varbs_j)
+        return _dot_product(_vec(u), varbs_j)
     else:
         X = M[:i + 1]
-        X_cols = [X[:,i] for i in range(n)]
+        X_cols = [X[:,i] for i in range(j + 1)]
         u = map(lambda x: _dot_product(varbs_i, x), X_cols)
-        return _dot_product(_vec(u[:j + 1]), varbs_j)
+        return _dot_product(_vec(u), varbs_j)
 
 # Given the polynomial ring P the dimension n, construct the top part of the 
 # matrix to determine the cone conditions. 
@@ -57,6 +57,39 @@ def _build_mat(P, n, UT=True):
         M = M[::-1]
     return map(merge, M)
 
+def _Gaussian_step(P, v, conds=[], factor=1, UT=True):
+    if not UT:
+        v = v[::-1]
+    n = len(v)
+    i = 0 
+    # find the "first" nonzero entry
+    while v[i] == 0:
+        i += 1
+        if i >= n:
+            return conds
+    if UT:
+        varbs_i = list(_row_vector_vars(P, n, i))
+    else:
+        varbs_i = list(_row_vector_vars(P, n, n - i - 1, UT=False))
+        varbs_i = varbs_i[::-1]
+    if (factor*varbs_i[0]).divides(v[i]):
+        # The first nonzero term is already divisible by factor * first var. 
+        q = P(v[i]/(factor*varbs_i[0]))
+        r = 1
+        new_conds = conds
+        new_factor = factor
+    else:
+        # The first nonzero term is not a multiple of the first var. 
+        q = v[i]
+        r = varbs_i[0]
+        new_conds = conds + [tuple([factor*r, q])]
+        new_factor = factor*r
+    v_new = v[:i] + map(lambda x: r*x[0] - q*x[1], zip(v[i:], varbs_i))
+    if not UT:
+        v_new = v_new[::-1]
+    # Recurse
+    return _Gaussian_step(P, v_new, conds=new_conds, factor=new_factor, UT=UT) 
+
 # Given a tensor as a sequence of matrices, return the cone conditions 
 # (assuming upper triangular).
 def ConeConditions(t, upper_triangular=True):
@@ -69,5 +102,7 @@ def ConeConditions(t, upper_triangular=True):
     vecs = [[_multiply_rows(P, i, j, t[k], UT=up) 
         for k in range(m)] for i in range(n) for j in range(i, n)]
     top = _build_mat(P, n, UT=up)
-    print vecs
-    return _Mat(top + vecs)
+
+    _add = lambda x, y: x + y
+    M = _Mat(top + vecs)
+    return reduce(_add, map(lambda x: _Gaussian_step(P, x, UT=up), vecs), []), M
